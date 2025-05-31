9d7f5fc60a1a32f5362c015176782c56b6d7fbb1
Cloud3654 <163625776+Cloud3654@users.noreply.github.com>
2025-05-31 14:48:14 +0900

    Create ^8335 (P5): 포스터 붙이기.cpp

---
+++ ^8335 (P5): 포스터 붙이기.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int cnt; cin >> cnt;
+    vector<pair<int, int>> b(cnt); //first, second = count, height
+    for (auto& p : b) cin >> p.first >> p.second();
+    
+}
