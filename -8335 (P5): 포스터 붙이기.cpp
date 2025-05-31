be67abb469d2c5f8bb3309e83397db9c86569fcd
Cloud3654 <163625776+Cloud3654@users.noreply.github.com>
2025-05-31 15:51:25 +0900

    Update -8335 (P5): 포스터 붙이기.cpp

--- -8335 (P5): 포스터 붙이기.cpp
+++ -8335 (P5): 포스터 붙이기.cpp
@@ -13,10 +13,16 @@ void loop() {
         else p -= min_h;
     }
 }
+void prt() {
+    for (auto p : h) cout << p << ' '; cout << ": " << poster << endl;
+}
 int main() {
     int cnt; cin >> cnt, b.resize(cnt);
     for (auto& p : b) cin >> p.first >> p.second;
     for (auto p : b) h.push_back(p.second);
-    while (*max_element(h.begin(), h.end())) loop();
+    while (*max_element()) {
+        loop();
+        prt();
+    }
     cout << poster;
 }
